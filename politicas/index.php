e20c31f24af1c6075fb3f651bb61e44506ad5148
Diego Rubiano <diegorubiano@dynamikcollective.com>
2021-03-26 11:22:50 -0500

    politicas de uso

---
+++ politicas/index.php
@@ -0,0 +1,27 @@
+<script> 
+    $section = 'politicas';  
+    $sectionTitle= 'DOMUS | San patricio - Politica de tratamiento de datos'; 
+</script>
+
+<?php 
+    require_once '../includes-web/header.php';
+?>
+
+
+<section id="apartments">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-5 mt-5 pt-3">
+                <h4 class="title-type-01">Politica de privacidad</h4>
+                <p class="text-type-01">
+                <br>
+                    <a href='http://proyectodomus.com/POLITICAS_PRIVACIDAD.pdf'>Politica de privacidad</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php 
+    require_once 'includes/footer.php';
+?>
