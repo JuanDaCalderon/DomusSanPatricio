85ad48defdf3f4fe3b63d670e2e3fd3d77ee5526
Juanda <juandacalji@gmail.com>
2021-04-27 14:55:03 -0500

    Footer terms fix

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
             </a>
         </p>-->
         <p class="float-right">
-            <a class="floor-plans-access-button open" href="<?php echo $base_url ?>politicas">TÉRMINOS Y POLITICAS </a>
+            <a class="floor-plans-access-button open" href="<?php echo $base_url ?>/politicas">TÉRMINOS Y POLITICAS </a>
             <span>| DOMUS</span> SAN PATRICIO | CALLE 106 # 17 - 73/85 | Imagenes de referencia. El diseño puede cambiar.
         </p>
 
